ce706784cd3f1dc87e205f8656f91d14145bc0b3
Michael Rehn <rehn.michael@outlook.com>
2014-10-10 17:28:42 +0200

    Updated gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,20 @@
 var gulp = require('gulp');
 var serve = require('gulp-serve');
+var watch = require('gulp-watch');
+var connect = require('connect');
+var livereload = require('gulp-livereload');
 
-gulp.task('serve', serve('src'));
+gulp.task('server', function(next) {
+    var server = connect();
+
+    server.use(connect.static('src', {default: 'index.html'})).listen(process.env.PORT || 3000, next);
+});
+
+gulp.task('serve', ['server'], function() {
+    var server = livereload();
+
+    livereload.listen();
+    gulp.watch('src/**/*.*').on('change', function(file) {
+        server.changed(file.path);
+    });
+});
