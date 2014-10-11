eec91f7689eb988d5a8096832d096b4a01d9c0ce
Michael Rehn <rehn.michael@outlook.com>
2014-10-11 23:24:43 +0200

    Added gulp-inject

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync');
+var inject = require('gulp-inject');
 
 gulp.task('browser-sync', function() {
     browserSync({
@@ -13,7 +14,17 @@ gulp.task('browser-sync-reload', function() {
     browserSync.reload();
 });
 
+gulp.task('js', function() {
+    var target = gulp.src('./index.html', {cwd: './src'});
+
+    var sources = gulp.src('./js/**/*.js', {read: false, cwd: './src'});
+
+    return target.pipe(inject(sources)).pipe(gulp.dest('./src'));
+
+});
+
 gulp.task('serve', ['browser-sync'], function() {
-    gulp.watch(['**/*.js', '**/*.html'], ['browser-sync-reload'], {cwd: './src'});
+    gulp.watch('./src/**/*.js', ['js', 'browser-sync-reload']);
+    gulp.watch('./src/**/*.html', ['browser-sync-reload']);
 });
 
