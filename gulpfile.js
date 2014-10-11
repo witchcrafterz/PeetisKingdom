de5819880b9a8521fb88e86c98ea8beed49d53ce
Michael Rehn <rehn.michael@outlook.com>
2014-10-11 23:40:12 +0200

    Now injecting vendor files with main-bower-files

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync');
 var inject = require('gulp-inject');
+var bowerFiles = require('main-bower-files');
+
 
 gulp.task('browser-sync', function() {
     browserSync({
@@ -14,17 +16,26 @@ gulp.task('browser-sync-reload', function() {
     browserSync.reload();
 });
 
-gulp.task('js', function() {
+function getJS() {
+    return gulp.src('./js/**/*.js', {read: false, cwd: './src'});
+}
+
+function getVendors() {
+    console.log(bowerFiles());
+    return gulp.src(bowerFiles(), {read: false, cwd: './src'});
+}
+
+gulp.task('inject', function() {
     var target = gulp.src('./index.html', {cwd: './src'});
 
-    var sources = gulp.src('./js/**/*.js', {read: false, cwd: './src'});
-
-    return target.pipe(inject(sources)).pipe(gulp.dest('./src'));
-
+    return target
+        .pipe(inject(getVendors(), {name: 'bower'}))
+        .pipe(inject(getJS()))
+        .pipe(gulp.dest('./src'));
 });
 
-gulp.task('serve', ['browser-sync'], function() {
-    gulp.watch('./src/**/*.js', ['js', 'browser-sync-reload']);
+gulp.task('serve', ['inject', 'browser-sync'], function() {
+    gulp.watch('./src/**/*.js', ['inject', 'browser-sync-reload']);
     gulp.watch('./src/**/*.html', ['browser-sync-reload']);
 });
 
