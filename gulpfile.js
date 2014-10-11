19c86b901895daf9b7bd11a3eeed0ba30aca6d25
Michael Rehn <rehn.michael@outlook.com>
2014-10-11 23:18:34 +0200

    Now using browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,19 @@
 var gulp = require('gulp');
-var serve = require('gulp-serve');
-var watch = require('gulp-watch');
-var connect = require('connect');
-var livereload = require('gulp-livereload');
+var browserSync = require('browser-sync');
 
-gulp.task('server', function(next) {
-    var server = connect();
-
-    server.use(connect.static('src', {default: 'index.html'})).listen(process.env.PORT || 3000, next);
-});
-
-gulp.task('serve', ['server'], function() {
-    var server = livereload();
-
-    livereload.listen();
-    gulp.watch('src/**/*.*').on('change', function(file) {
-        server.changed(file.path);
+gulp.task('browser-sync', function() {
+    browserSync({
+        server: {
+            baseDir: './src'
+        }
     });
 });
+
+gulp.task('browser-sync-reload', function() {
+    browserSync.reload();
+});
+
+gulp.task('serve', ['browser-sync'], function() {
+    gulp.watch(['**/*.js', '**/*.html'], ['browser-sync-reload'], {cwd: './src'});
+});
+
