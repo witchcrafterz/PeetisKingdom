6754ab712d5584f5af3501526ce111ee8e8a0c7f
unknown <SiHy1501@SSMLT-ND4326B77.ad.grillska.se>
2014-10-15 10:30:15 +0200

    Added dandy background music

--- src/js/Game/level/level.js
+++ src/js/Game/level/level.js
@@ -3,6 +3,7 @@
 
     Game.level = function(game) {
     };
+        var music;
 
     Game.level.prototype = {
 
@@ -12,7 +13,10 @@
         preload: function() {
             this.game.load.spritesheet('player1', 'assets/player1.png', 80, 97);
             this.game.load.image('bg', 'assets/bg_water.jpg');
+
+            this.game.load.audio('jorm', 'assets/jatten_jorm.wav');
         },
+
 
         /**
          * Initialization logic here
@@ -26,6 +30,9 @@
             var p1 = this.p1 = new Game.player(this.game, this.game.world.centerX, this.game.world.centerY);
             this.game.add.existing(p1);
 
+            music = this.game.add.audio('jorm');
+
+            music.fadeIn(1000, true);
         },
 
         update: function() {
