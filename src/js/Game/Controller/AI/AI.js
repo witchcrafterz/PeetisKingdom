32f54d8966b7c3b00ac18ff285fe05f4329b25ea
Michael Rehn <rehn.michael@outlook.com>
2014-12-11 18:31:35 +0100

    The AI has learned to fling you!

--- src/js/Game/Controller/AI/AI.js
+++ src/js/Game/Controller/AI/AI.js
@@ -15,13 +15,23 @@
     Game.Controller.AI.prototype.constructor = Game.Controller.AI;
 
     Game.Controller.AI.prototype.update = function(toTrack) {
-        if (toTrack.position.x - this.controlled.position.x > 500) {
+        if (this.game.physics.arcade.intersects(toTrack.body, this.controlled.body)) {
+
+            var dir = toTrack.position.x > this.controlled.position.x ? 1 : -1;
+
+            toTrack.body.position.y -= 5;
+            toTrack.body.velocity.setTo(1500 * dir, -1500);
+        } else {
+
+        }
+
+        if (toTrack.position.x > this.controlled.position.x) {
             this.right.setDown.call(this);
         } else {
             this.right.setUp.call(this);
         }
 
-        if (toTrack.position.x - this.controlled.position.x < -500) {
+        if (toTrack.position.x < this.controlled.position.x) {
             this.left.setDown.call(this);
         } else {
             this.left.setUp.call(this);
