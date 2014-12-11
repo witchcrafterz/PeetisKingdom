7914793d34a85729ac4fc5d025efd805fec05037
Michael Rehn <rehn.michael@outlook.com>
2014-12-11 18:29:55 +0100

    Extended math with a clamp function

--- src/js/Game.js
+++ src/js/Game.js
@@ -1,6 +1,10 @@
 (function() {
     'use strict';
 
+    Math.clamp = function(value, lower, upper) {
+        return Math.max(Math.min(value, upper), lower);
+    };
+
     /**
      * The main game namespace
      * @namespace
