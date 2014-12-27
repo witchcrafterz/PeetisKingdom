2262e5bc5e2a417bfd8e675a835bce32adcccfdb
Michael Rehn <rehn.michael@outlook.com>
2014-12-27 17:03:41 +0100

    Added a random noise bg

--- src/js/Game/MainMenu/MainMenu.js
+++ src/js/Game/MainMenu/MainMenu.js
@@ -18,6 +18,13 @@
             return;
         }
 
+        var bmd = this.game.make.bitmapData(this.game.width, this.game.height);
+        bmd.processPixelRGB(this.noise, this);
+        bmd.dirty = true;
+
+        this.bg = this.game.add.image(0, 0, bmd);
+        this.bg.bmd = bmd;
+
         this.welcomeText = this.game.add.bitmapText(this.game.width * 0.5, this.game.height * 0.25, 'font', 'Welcome to the spel!', 72);
         this.welcomeText.position.x -= this.welcomeText.width * 0.5;
         this.welcomeText.position.y -= this.welcomeText.height * 0.5;
@@ -29,6 +36,17 @@
 
     Game.MainMenu.prototype.startLevel = function() {
         this.state.start('Game.Level');
+    };
+
+    Game.MainMenu.prototype.noise = function(pixel) {
+        var val = ((50 * Math.random())|0);
+
+        pixel.r = val;
+        pixel.g = val;
+        pixel.b = val;
+        pixel.a = 50 + val;
+
+        return pixel;
     };
 
 })();
