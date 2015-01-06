31ff8fd268558d2c497929087d8dfc08f1252929
Michael Rehn <rehn.michael@outlook.com>
2015-01-07 00:13:42 +0100

    Enabled quad tree as it seemed to give a marginal performance boost

--- src/js/Game/Level/Level.js
+++ src/js/Game/Level/Level.js
@@ -575,6 +575,7 @@
         this.game.physics.startSystem(Phaser.Physics.ARCADE);
         this.game.physics.arcade.gravity = Game.gravity;
         this.game.physics.arcade.TILE_BIAS = 70;
+        this.game.physics.arcade.skipQuadTree = false;
 
         this.activateKey = this.game.input.keyboard.addKey(Phaser.Keyboard.UP);
 
