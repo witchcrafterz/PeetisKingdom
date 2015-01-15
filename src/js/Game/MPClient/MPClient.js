dff57a9ac3e45c7a8f31951be15d5ee84808067a
Michael Rehn <rehn.michael@outlook.com>
2015-01-15 23:06:28 +0100

    Updated MPClient to support the new action protocol

--- src/js/Game/MPClient/MPClient.js
+++ src/js/Game/MPClient/MPClient.js
@@ -37,6 +37,9 @@
          */
         this.characters = {};
 
+        this.state = this.game.state.getCurrentState();
+        this.p1 = this.state.p1;
+
         this.connect();
         this.setEvents();
     };
@@ -48,16 +51,14 @@
     };
 
     Game.MPClient.prototype.sendPosition = function() {
-        var p1 = this.game.state.getCurrentState().p1;
-
         var pos = {
-            x: p1.position.x,
-            y: p1.position.y
+            x: this.p1.position.x,
+            y: this.p1.position.y
         };
 
         this.socket.emit('position', { position: pos });
 
-        setTimeout(bind(this, this.sendPosition), 50);
+        setTimeout(bind(this, this.sendPosition), 500);
     };
 
     Game.MPClient.prototype.setEvents = function() {
@@ -65,18 +66,53 @@
         this.socket.on('new', bind(this, this.onNew));
         this.socket.on('delete', bind(this, this.onDelete));
         this.socket.on('position', bind(this, this.onPosition));
+        this.socket.on('action', bind(this, this.onAction));
+
+        this.bindController();
+    };
+    
+    Game.MPClient.prototype.bindController = function() {
+        this.p1.controller.down.onDown.add(this.sendController, this);
+        this.p1.controller.down.onUp.add(this.sendController, this);
+
+        this.p1.controller.left.onDown.add(this.sendController, this);
+        this.p1.controller.left.onUp.add(this.sendController, this);
+
+        this.p1.controller.right.onDown.add(this.sendController, this);
+        this.p1.controller.right.onUp.add(this.sendController, this);
+
+        this.p1.controller.jump.onDown.add(this.sendController, this);
+        this.p1.controller.jump.onUp.add(this.sendController, this);
+    };
+
+    Game.MPClient.prototype.unbindController = function() {
+        this.p1.controller.down.onDown.remove(this.sendController, this);
+        this.p1.controller.down.onUp.remove(this.sendController, this);
+
+        this.p1.controller.left.onDown.remove(this.sendController, this);
+        this.p1.controller.left.onUp.remove(this.sendController, this);
+
+        this.p1.controller.right.onDown.remove(this.sendController, this);
+        this.p1.controller.right.onUp.remove(this.sendController, this);
+
+        this.p1.controller.jump.onDown.remove(this.sendController, this);
+        this.p1.controller.jump.onUp.remove(this.sendController, this);
+    };
+
+    Game.MPClient.prototype.sendController = function(key, state) {
+        var action = {};
+        action[key] = state;
+        this.socket.emit('action', action);
     };
 
     Game.MPClient.prototype.onID = function(data) {
         this.id = data.id;
 
-        var state = this.game.state.getCurrentState();
-        var p1 = state.p1;
         var pos = {
-            x: p1.position.x,
-            y: p1.position.y
+            x: this.p1.position.x,
+            y: this.p1.position.y
         };
-        var key = p1.key;
+        var key = this.p1.key;
 
         this.socket.emit('new', { position: pos, texture: key });
         this.sendPosition();
@@ -112,11 +148,39 @@
         character.position.setTo(data.position.x, data.position.y);
     };
 
+    Game.MPClient.prototype.onAction = function(data) {
+
+        _.forEach(data, function(actions, id) {
+            if (id === this.id) return;
+            var character = this.characters[id];
+
+            _.forEach(actions, function(action) {
+                var type = _.keys(action)[0];
+                var value = action[type];
+                switch (type) {
+                    case 'left':
+                    case 'up':
+                    case 'down':
+                    case 'right':
+                    case 'jump':
+                        if (value) {
+                            character.controller[type].setDown.call(character.controller);
+                        } else {
+                            character.controller[type].setUp.call(character.controller);
+                        }
+                        break;
+                }
+            });
+        }, this);
+
+    };
+
     Game.MPClient.prototype.disconnect = function() {
         _.forEach(this.characters, function(character) {
             this.game.entitiesGroup.remove(character, true);
         }, this);
 
+        this.unbindController();
         this.characters = null;
 
         this.socket.disconnect();
