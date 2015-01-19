f5efcf76c334209b2e5c116d0e0dadb95429b762
Michael Rehn <rehn.michael@outlook.com>
2015-01-19 19:30:52 +0100

    Torches can now have a tint of a colour

--- src/js/Game/Shadow/Shadow.js
+++ src/js/Game/Shadow/Shadow.js
@@ -17,8 +17,8 @@
     Game.Shadow.prototype.constructor = Game.Shadow;
 
 
-    Game.Torch = function(game, x, y, radius, fade) {
-        this.torchTexture = torchCache[radius + '' + fade] ? torchCache[radius] : Game.Torch.createRadial(game, radius, fade);
+    Game.Torch = function(game, x, y, radius, fade, r, g, b) {
+        this.torchTexture = torchCache[radius + '' + fade] ? torchCache[radius] : Game.Torch.createRadial(game, radius, fade, r, g, b);
         Phaser.Image.call(this, game, x, y, this.torchTexture);
 
         this.anchor.setTo(0.5);
@@ -30,17 +30,20 @@
     Game.Torch.prototype = Object.create(Phaser.Image.prototype);
     Game.Torch.prototype.constructor = Game.Torch;
 
-    Game.Torch.createRadial = function(game, radius, fade) {
+    Game.Torch.createRadial = function(game, radius, fade, r, g, b) {
         radius = radius || 200;
         fade = fade || 0.1;
+        r = r || 255;
+        g = g || 150;
+        b = b || 100;
 
         var bitmapData = new Phaser.BitmapData(game, 'radial' + radius, radius * 2, radius * 2);
 
         var gradient = bitmapData.context.createRadialGradient(
             radius, radius, radius * fade,
             radius, radius, radius);
-        gradient.addColorStop(0, 'rgba(255, 255, 255, 1.0)');
-        gradient.addColorStop(1, 'rgba(255, 255, 255, 0.0)');
+        gradient.addColorStop(0, 'rgba({0}, {1}, {2}, 1.0)'.format(r, g, b));
+        gradient.addColorStop(1, 'rgba({0}, {1}, {2}, 0.0)'.format(r, g, b));
 
         bitmapData.context.beginPath();
         bitmapData.context.fillStyle = gradient;
