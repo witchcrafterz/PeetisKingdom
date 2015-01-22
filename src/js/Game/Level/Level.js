d5a345b9c655aab005cf166d32268f53459ba05a
Michael Rehn <rehn.michael@outlook.com>
2015-01-22 18:58:15 +0100

    Re-introduced tile bias as tunneling issue were still prevalent; however it now has a lower value than before

--- src/js/Game/Level/Level.js
+++ src/js/Game/Level/Level.js
@@ -616,6 +616,7 @@
     Game.Level.prototype.create = function() {
         this.game.physics.startSystem(Phaser.Physics.ARCADE);
         this.game.physics.arcade.gravity = Game.gravity;
+        this.game.physics.arcade.TILE_BIAS = 32;
 
         this.activateKey = this.game.input.keyboard.addKey(Phaser.Keyboard.UP);
 
