d648144024ef5b8488ac65bc5121df06f963da05
Michael Rehn <rehn.michael@outlook.com>
2015-01-29 23:34:44 +0100

    Added textures for guard and peeti and their characters are now using those textures in the tile map

--- src/js/Game/Level/Level.js
+++ src/js/Game/Level/Level.js
@@ -154,6 +154,8 @@
         this.game.load.image('sidecut', 'assets/characters/sidecut.png');
         this.game.load.image('florence', 'assets/characters/florence.png');
         this.game.load.image('mamabirdie', 'assets/characters/mamabirdie.png');
+        this.game.load.image('peeti', 'assets/characters/peeti.png');
+        this.game.load.image('guard', 'assets/characters/guard.png');
 
         this.game.load.spritesheet('torch', 'assets/items/torch.png', 16, 52);
         this.game.load.image('batteria', 'assets/items/batteria.png');
